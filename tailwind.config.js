7885c6e6a727ac6f43fc40270edb2aba3946b213
Edgar A. Espinosa <edgarespinosa@outlook.com>
2024-05-17 13:43:15 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'navy': '#393053',
+        'midnight': '#443C68',
+      },
+    },
   },
   plugins: [],
 }
