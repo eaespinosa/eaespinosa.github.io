1a92bb4cfedc93fa5f02ecbaf99e0cff57e58070
Edgar A. Espinosa <edgarespinosa@outlook.com>
2024-05-20 18:43:30 -0600

    added multilingual functionality and made some minor updates

--- src/main.js
+++ src/main.js
@@ -38,3 +38,56 @@ themeToggleBtn.addEventListener('click', function() {
     }
     
 });
+
+// Function to update content based on selected language
+function updateContent(langData) {
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        element.textContent = langData[key];
+    });
+}
+
+// Function to set the language preference
+function setLanguagePreference(lang) {
+    localStorage.setItem('language', lang);
+    location.reload();
+}
+
+// Function to fetch language data
+async function fetchLanguageData(lang) {
+    const response = await fetch(`${lang}.json`);
+    return response.json();
+}
+
+// Function to change language
+async function changeLanguage(lang) {
+    await setLanguagePreference(lang);
+    
+    const langData = await fetchLanguageData(lang);
+    updateContent(langData);
+    toggleEnglishStylesheet(lang); // Toggle English stylesheet
+}
+
+// Function to toggle English stylesheet based on language selection
+function toggleEnglishStylesheet(lang) {
+    const head = document.querySelector('head');
+    const link = document.querySelector('#styles-link');
+
+    if (link) {
+        head.removeChild(link); // Remove the old stylesheet link
+    } else if (lang === 'en') {
+        const newLink = document.createElement('link');
+        newLink.id = 'styles-link';
+        newLink.rel = 'stylesheet';
+        newLink.href = 'style-en.css'; // Path to English stylesheet
+        head.appendChild(newLink);
+    }
+}
+
+// Call updateContent() on page load
+window.addEventListener('DOMContentLoaded', async () => {
+    const userPreferredLanguage = localStorage.getItem('language') || 'es';
+    const langData = await fetchLanguageData(userPreferredLanguage);
+    updateContent(langData);
+    toggleEnglishStylesheet(userPreferredLanguage);
+});
